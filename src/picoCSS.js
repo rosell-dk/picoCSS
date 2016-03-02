1158ae227f1e3eb8fa29aae03d100ed72293611a
Bjørn Rosell <it@rosell.dk>
2016-03-02 13:19:11 +0100

    Made chainable and easier to do custom "each"
    https://www.blogger.com/comment.g?blogID=20880701&postID=5733534663763128915&page=1

--- src/picoCSS.js
+++ src/picoCSS.js
@@ -1,34 +1,35 @@
 var p = {
-
-    select: function (selector) {
-        this.value = Array.prototype.slice.call(document.querySelectorAll(selector));
-        return this;
-    },
-    each: [].forEach,
-    css: function (v) {
-        this.value = this.each.call(this.value, function (i) {
-            i.style.cssText = v;
-        });
-        return this;
-    },
-    att: function (a, v) {
-        this.value = this.each.call(this.value, function (i) {
-            i.setAttribute(a, v);
-        });
-        return this;
-    },
-    animate: function (time, scale, rotate, rotateX, rotateY, translateX, translateY, skewX, skewY) {
-        this.value = [].forEach.call(this.value, function (i) {
-            i.style.cssText = '-webkit-transition: all ' + time + 's ease-in-out; -webkit-transform: scale(' + scale + ') rotate(' + rotate + 'deg) rotateX(' + rotateX + 'deg) rotateY(' + rotateY + 'deg) translate(' + translateX + 'px, ' + translateY + 'px) skew(' + skewX + 'deg, ' + skewY + 'deg)';
-        });
-        return this;
-    },
-
-    on: function (type, fn) {
-        this.value = this.each.call(this.value, function (i) {
-            i.addEventListener(type, fn, false);
-        });
-        return this;
-    }
-
+  select: function (selector) {
+      this.value = Array.prototype.slice.call(document.querySelectorAll(selector));
+      return this;
+  },
+  each: function(fn) {
+    [].forEach.call(this.value, fn);
+    return this;
+  },
+  css: function (v) {
+    return this.each(function (i) {
+      i.style.cssText = v;
+    });
+  },
+  att: function (a, v) {
+    return this.each(function (i) {
+      i.setAttribute(a, v);
+    });
+  },
+  on: function (type, fn) {
+    return this.each(function (i) {
+      i.addEventListener(type, fn, false);
+    });
+  },
+  addClass: function (v) {
+    return this.each(function (i) {
+      if (i.classList) {
+        i.classList.add(v)
+      } 
+      else {
+        i.className += ' ' + v;
+      }
+    });
+  },
 };
